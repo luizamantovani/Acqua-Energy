2a0fc3e6a11d05884ff3caf5cab0d70748c0ce63
Gabriel Rodrigues <gabrieldsrodrigues19@gmail.com>
2024-11-04 23:35:19 -0300

    Adiciona botão de alternância de tema e estilos

--- script.js
+++ script.js
@@ -474,3 +474,14 @@ dropdowns.forEach(dropdown => {
         });
     });
 });
+
+document.getElementById('toggleTheme').addEventListener('click', () => {
+    const currentTheme = document.getElementById('themeStylesheet').getAttribute('href');
+    if (currentTheme === 'style.css') {
+        document.getElementById('themeStylesheet').setAttribute('href', 'style-escuro.css');
+        document.getElementById('toggleTheme').innerText = 'Modo Claro';
+    } else {
+        document.getElementById('themeStylesheet').setAttribute('href', 'style.css');
+        document.getElementById('toggleTheme').innerText = 'Modo Escuro';
+    }
+});
